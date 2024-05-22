c7b8a4bbe0eb94b45859898b897e8a540e3ea699
Dexaf <federicopisajob@gmail.com>
2024-05-22 22:05:00 +0200

    moved functions away from main

---
+++ utils.h
@@ -0,0 +1,8 @@
+#include <vector>
+#include "storageItem.h"
+
+#ifndef UTILS
+#define UTILS
+std::vector<storageItemSpace::storageItem> loadStorageItemsFromFile(std::istream& file);
+#endif // !UTILS
+
